2e6ad19ef487659e974539902c9e6473aa8f4ecb
Owor Wilbroad <owwilly4by4@gmail.com>
2023-06-16 00:12:25 +0300

    Code cleaning

--- views/dashboard/parts/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
-        <p class="text-muted text-center text-md-left">Copyright © <?= date('Y');?> <a href="#"
-            target="_blank"> Owor Wilbroad, Reg: 2022/HD05/1450U </a>. All rights reserved</p>
-      </footer>
